123e3784b7dc06432782821d42f286177032d3a9
EVissI <vissarionoffe@yandex.ru>
2024-02-26 22:41:15 +0300

    integration openAi

--- src/main/kotlin/openai/request/OpenAIRequests.kt
+++ src/main/kotlin/openai/request/OpenAIRequests.kt
@@ -3,17 +3,16 @@ package openai.request
 import com.aallam.openai.api.edits.EditsRequest
 import com.aallam.openai.api.model.ModelId
 import config.OpenAIClient
-import io.ktor.client.*
 
 
 class OpenAIRequests {
-    suspend fun generatePrompt(promt:String) : String{
+    suspend fun generatePrompt(prompt:String) : String{
         val openAIClient = OpenAIClient().getClient()
 
         val edit =  openAIClient.edit(
             request = EditsRequest(
                 model = ModelId("text-davinci-edit-001"),
-                input = "What day of the wek is it?",
+                input = prompt,
                 instruction = "Fix the spelling mistakes"
             )
         )
